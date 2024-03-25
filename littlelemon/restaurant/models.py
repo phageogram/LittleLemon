c31c4591230caaca447808e3d40f5a24bf66a6e2
phageogram <phageogram@gmail.com>
2024-03-25 17:27:14 -0600

    create and migrate Booking and Menu models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.AutoField(
+        primary_key=True,  
+        verbose_name="ID",
+        )
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+class Menu(models.Model):
+    id = models.AutoField(
+        primary_key=True,
+        verbose_name="ID"
+        )
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    inventory = models.IntegerField()
