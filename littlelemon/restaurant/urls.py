b371e6186ad2bf6e778637b7b6ea318f55d5bee7
phageogram <phageogram@gmail.com>
2024-03-25 21:48:10 -0600

    added test case

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
     path('',TemplateView.as_view(template_name='index.html'), name='indexpage'),
     path('items/', views.MenuItemsView.as_view()),
     path('items/<int:pk>', views.SingleMenuItemView.as_view()),
+    #path('message/', views.msg),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -4,6 +4,9 @@ from .serializers import MenuSerializer, BookingSerializer
 from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.authentication import TokenAuthentication
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
@@ -15,6 +18,7 @@ class MenuItemsView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
@@ -22,3 +26,9 @@ class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = [IsAuthenticated]
+
+#@api_view()
+#@authentication_classes([TokenAuthentication])
+#@permission_classes([IsAuthenticated])
+#def msg(request):
+#    return JsonResponse({'message': 'This view is protected'})
